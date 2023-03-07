44022876e8d455a34746cbcb4295c5a2be953cdc
Connor Payne <connorpayne1998@gmail.com>
2023-03-07 09:19:35 +0000

    Inital content for setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='iris-cookiecase-module',
+    python_requires='>=3.9',
+    version='0.1.0',
+    packages=['iris_cookiecase_module'],
+    url='https://github.com/LoneWolf-96/iris-cookiecase-module',
+    license='MIT',
+    author='iris-cookiecase-module',
+    author_email='coding.LoneWolf-96@proton.me',
+    description='`iris-cookiecase-module` is a IRIS processor module designed to set out the core note groups and notes required when a new case is created.',
+    install_requires=[]
+)
